23ffbbe59dde390a4c1f205f788b71a356b1f4ee
Sourabh Vanshkar <sourabh.vanshkar_cs21@gmail.com>
2024-06-06 02:47:10 +0530

    Updates

---
+++ precache-manifest.70846ce63a36e6c1449fc3a53a50c108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3c85f19baaf0c7beb84",
+    "url": "/Personal-Bookshelf/static/css/main.f3c4d8bb.chunk.css"
+  },
+  {
+    "revision": "b3c85f19baaf0c7beb84",
+    "url": "/Personal-Bookshelf/static/js/main.957a4192.chunk.js"
+  },
+  {
+    "revision": "37fd3da8d858951c6017",
+    "url": "/Personal-Bookshelf/static/js/runtime~main.72ed6c9f.js"
+  },
+  {
+    "revision": "07a797a021e99b0a13d1",
+    "url": "/Personal-Bookshelf/static/js/2.931e2c01.chunk.js"
+  },
+  {
+    "revision": "4549b272a3239dc43a82",
+    "url": "/Personal-Bookshelf/static/js/3.8f58f390.chunk.js"
+  },
+  {
+    "revision": "48436032d28f820fbab86578409da364",
+    "url": "/Personal-Bookshelf/index.html"
+  }
+];
